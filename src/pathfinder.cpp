d59cb8832f23ecb22423c37d428aede91250bdca
orion40 <qolizion@protonmail.com>
2018-03-19 16:03:58 +0100

    Correct braces

--- src/pathfinder.cpp
+++ src/pathfinder.cpp
@@ -12,72 +12,74 @@
 
 class pathfinder {
 
-private:
-    ros::NodeHandle n;
+    private:
+        ros::NodeHandle n;
 
-    ros::Subscriber sub_scan;
-    ros::Subscriber sub_robot_moving;
+        ros::Subscriber sub_scan;
+        ros::Subscriber sub_robot_moving;
 
-    ros::Publisher pub_moving_persons_detector;
-    ros::Publisher pub_moving_persons_detector_marker;
+        ros::Publisher pub_moving_persons_detector;
+        ros::Publisher pub_moving_persons_detector_marker;
 
-    // to store, process and display laserdata
-    //geometry_msgs::Point 
+        // to store, process and display laserdata
+        //geometry_msgs::Point 
 
-    //pour stocker chemin
-    geometry_msgs::Point point[NBPOINTS];
-    int pathToDo[PATHLENGTH];
-    int currentpoint=0;
+        //pour stocker chemin
+        geometry_msgs::Point point[NBPOINTS];
+        int pathToDo[PATHLENGTH];
+        int currentpoint=0;
 
-public:
+    public:
 
-pathfinder() {
+        pathfinder() {
 
 
-   /*// communication with cmd_vel
-    pub_cmd_vel = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+            /*// communication with cmd_vel
+              pub_cmd_vel = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 
-    // communication with odometry
-    sub_odometry = n.subscribe("odom", 1, &translation_action::odomCallback, this);
-    cond_translation = false;
+            // communication with odometry
+            sub_odometry = n.subscribe("odom", 1, &translation_action::odomCallback, this);
+            cond_translation = false;
 
-    // communication with decision
-    pub_translation_done = n.advertise<std_msgs::Float32>("translation_done", 1);
-    sub_translation_to_do = n.subscribe("translation_to_do", 1, &translation_action::translation_to_doCallback, this);//this is the translation that has to be performed
+            // communication with decision
+            pub_translation_done = n.advertise<std_msgs::Float32>("translation_done", 1);
+            sub_translation_to_do = n.subscribe("translation_to_do", 1, &translation_action::translation_to_doCallback, this);//this is the translation that has to be performed
 
-    // communication with obstacle_detection
-    sub_obstacle_detection = n.subscribe("closest_obstacle", 1, &translation_action::closest_obstacleCallback, this);
+            // communication with obstacle_detection
+            sub_obstacle_detection = n.subscribe("closest_obstacle", 1, &translation_action::closest_obstacleCallback, this);
 
-    error_integral = 0;
-    error_previous = 0;
+            error_integral = 0;
+            error_previous = 0;
 
-    new_translation_to_do = false;
-    new_odom = false;
-    init_obstacle = false;
-*/
+            new_translation_to_do = false;
+            new_odom = false;
+            init_obstacle = false;
+            */
 
-    //BOUCLE POUR PASSER D'UN POINT A L'AUTRE
-    ros::Rate r(10);// this node will run at 10hz
-    while (currentpoint < PATHLENGTH-1) {
-        ros::spinOnce();//each callback is called once to collect new data
-        update();//processing of data
-        r.sleep();//we wait if the processing (ie, callback+update) has taken less than 0.1s (ie, 10 hz)
-    }
+            //BOUCLE POUR PASSER D'UN POINT A L'AUTRE
+            ros::Rate r(10);// this node will run at 10hz
+            while (currentpoint < PATHLENGTH-1) {
+                ros::spinOnce();//each callback is called once to collect new data
+                update();//processing of data
+                r.sleep();//we wait if the processing (ie, callback+update) has taken less than 0.1s (ie, 10 hz)
+            }
 
-//UPDATE: main processing
-/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
-void update() {
-    geometry_msgs::Point targetpoint;
-	//while(currentpoint < PATHLENGTH-1){
-		targetpoint = pathToDo[currentpoint+1];
-		calcul_rotation(pathToDo[currentpoint],targetpoint); //TODO
-		faire_rotation(); //TODO
-		calcul_translation(pathToDo[currentpoint],targetpoint); //TODO
-		faire_translation(); //TODO
-		currentpoint++;
-	//}
+            //UPDATE: main processing
+            /*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+            void update() {
+                geometry_msgs::Point targetpoint;
+                //while(currentpoint < PATHLENGTH-1){
+                targetpoint = pathToDo[currentpoint+1];
+                calcul_rotation(pathToDo[currentpoint],targetpoint); //TODO
+                faire_rotation(); //TODO
+                calcul_translation(pathToDo[currentpoint],targetpoint); //TODO
+                faire_translation(); //TODO
+                currentpoint++;
+                //}
 
+            }
+        }
 }
 
 
