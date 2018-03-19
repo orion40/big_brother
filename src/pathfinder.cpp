a56331dde09fea7bb6e01dd9e3d092dd23f0364f
PizzaSansAnchois <moijaimelapizza@gmail.com>
2018-03-19 17:41:58 +0100

    distance entre 2 points

--- src/pathfinder.cpp
+++ src/pathfinder.cpp
@@ -49,27 +49,16 @@ class pathfinder {
             {
                 pathToDo[i]=i % NBPOINTS;
             }
-            /*// communication with cmd_vel
-              pub_cmd_vel = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 
-            // communication with odometry
-            sub_odometry = n.subscribe("odom", 1, &translation_action::odomCallback, this);
+            // communication with rotation_action
+            pub_rotation_to_do = n.advertise<std_msgs::Float32>("rotation_to_do", 0);
+            sub_rotation_done = n.subscribe("rotation_done", 1, &decision::rotation_doneCallback, this);
+            cond_rotation = false;
+        
+            // communication with translation_action
+            pub_translation_to_do = n.advertise<std_msgs::Float32>("translation_to_do", 0);
+            sub_translation_done = n.subscribe("translation_done", 1, &decision::translation_doneCallback, this);
             cond_translation = false;
-
-            // communication with decision
-            pub_translation_done = n.advertise<std_msgs::Float32>("translation_done", 1);
-            sub_translation_to_do = n.subscribe("translation_to_do", 1, &translation_action::translation_to_doCallback, this);//this is the translation that has to be performed
-
-            // communication with obstacle_detection
-            sub_obstacle_detection = n.subscribe("closest_obstacle", 1, &translation_action::closest_obstacleCallback, this);
-
-            error_integral = 0;
-            error_previous = 0;
-
-            new_translation_to_do = false;
-            new_odom = false;
-            init_obstacle = false;
-            */
 
             //BOUCLE POUR PASSER D'UN POINT A L'AUTRE
             ros::Rate r(10);// this node will run at 10hz
@@ -79,6 +68,7 @@ class pathfinder {
                 r.sleep();//we wait if the processing (ie, callback+update) has taken less than 0.1s (ie, 10 hz)
             }
         }
+
         //UPDATE: main processing
         /*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
@@ -98,12 +88,13 @@ class pathfinder {
 
             ROS_INFO("(pathfinder_node) waiting for /odometry and /map_server");
             ros::init(argc, argv, "pathfinder");
-
             pathfinder bsObject;
-
             ros::spin();
-
             return 0;
         }
 
+        // Distance between two points
+        float distancePoints(geometry_msgs::Point pa, geometry_msgs::Point pb) {
+            return sqrt(pow((pa.x-pb.x),2.0) + pow((pa.y-pb.y),2.0));
+        }
 }
